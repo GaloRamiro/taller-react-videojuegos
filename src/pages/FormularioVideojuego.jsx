6da37cfd412921740d7a3a6d26df123f265deab9
GaloRamiro <g_cisneros_23@hotmail.es>
2026-07-20 09:32:03 -0500

    feat: agregar validaciones y nuevos campos al formulario de videojuegos cambio de color en nav

--- src/pages/FormularioVideojuego.jsx
+++ src/pages/FormularioVideojuego.jsx
@@ -12,10 +12,12 @@ function FormularioVideojuego({ onGuardar }) {
       plataforma: "",
       lanzamiento: "",
       precio: "",
+      descripcion: "",
+      calificacion: "",
       disponible: false,
     },
   );
-
+  const [errores, setErrores] = useState({});
   function handleChange(e) {
     const { name, value, type, checked } = e.target;
 
@@ -31,7 +33,14 @@ function FormularioVideojuego({ onGuardar }) {
   }
   function handleSubmit(e) {
     e.preventDefault();
+    const erroresActivos = validarFormulario();
 
+    if (Object.keys(erroresActivos).length > 0) {
+      setErrores(erroresActivos);
+      return;
+    }
+
+    setErrores({});
     onGuardar(videojuego);
 
     navigate("/");
@@ -42,10 +51,61 @@ function FormularioVideojuego({ onGuardar }) {
       plataforma: "",
       lanzamiento: "",
       precio: "",
+      descripcion: "",
+      calificacion: "",
       disponible: false,
     });
   }
+  function validarFormulario() {
+    const erroresActivos = {};
 
+    // Título
+    if (videojuego.titulo.trim() === "") {
+      erroresActivos.titulo = "Ingrese un título.";
+    }
+
+    // Género
+    if (videojuego.genero === "") {
+      erroresActivos.genero = "Seleccione un género.";
+    }
+
+    // Plataforma
+    if (videojuego.plataforma.trim() === "") {
+      erroresActivos.plataforma = "Ingrese una plataforma.";
+    }
+
+    // Precio
+    if (Number(videojuego.precio) <= 0) {
+      erroresActivos.precio = "Ingrese un precio válido.";
+    }
+
+    // Fecha
+    const hoy = new Date().toISOString().split("T")[0];
+
+    if (videojuego.lanzamiento === "") {
+      erroresActivos.lanzamiento = "Seleccione una fecha.";
+    } else if (videojuego.lanzamiento > hoy) {
+      erroresActivos.lanzamiento = "La fecha no puede ser futura.";
+    }
+
+    // Descripción
+    if (videojuego.descripcion.trim().length < 10) {
+      erroresActivos.descripcion =
+        "La descripción debe tener al menos 10 caracteres.";
+    } else if (videojuego.descripcion.length > 250) {
+      erroresActivos.descripcion = "Máximo 250 caracteres.";
+    }
+
+    // Calificación
+    if (
+      Number(videojuego.calificacion) < 1 ||
+      Number(videojuego.calificacion) > 100
+    ) {
+      erroresActivos.calificacion = "Debe estar entre 1 y 100.";
+    }
+
+    return erroresActivos;
+  }
   return (
     <div className="formulario-container">
       <h1>{videojuegoEditar ? "Editar Videojuego" : "Registrar Videojuego"}</h1>
@@ -60,6 +120,9 @@ function FormularioVideojuego({ onGuardar }) {
             value={videojuego.titulo}
             onChange={handleChange}
           />
+          {errores.titulo && (
+            <span className="error-mensaje">{errores.titulo}</span>
+          )}
         </div>
 
         <div>
@@ -70,12 +133,14 @@ function FormularioVideojuego({ onGuardar }) {
             onChange={handleChange}
           >
             <option value="">Seleccione un género</option>
-            <option value="">Seleccione</option>
             <option value="Acción">Acción</option>
             <option value="RPG">RPG</option>
             <option value="Aventura">Aventura</option>
             <option value="Deportes">Deportes</option>
           </select>
+          {errores.genero && (
+            <span className="error-mensaje">{errores.genero}</span>
+          )}
         </div>
 
         <div>
@@ -87,6 +152,9 @@ function FormularioVideojuego({ onGuardar }) {
             value={videojuego.plataforma}
             onChange={handleChange}
           />
+          {errores.plataforma && (
+            <span className="error-mensaje">{errores.plataforma}</span>
+          )}
         </div>
 
         <div>
@@ -97,6 +165,9 @@ function FormularioVideojuego({ onGuardar }) {
             value={videojuego.lanzamiento}
             onChange={handleChange}
           />
+          {errores.lanzamiento && (
+            <span className="error-mensaje">{errores.lanzamiento}</span>
+          )}
         </div>
 
         <div>
@@ -109,8 +180,42 @@ function FormularioVideojuego({ onGuardar }) {
             value={videojuego.precio}
             onChange={handleChange}
           />
+          {errores.precio && (
+            <span className="error-mensaje">{errores.precio}</span>
+          )}
+        </div>
+        <div>
+          <label>Sinopsis / Descripción</label>
+
+          <textarea
+            name="descripcion"
+            placeholder="Escriba una breve descripción"
+            value={videojuego.descripcion}
+            onChange={handleChange}
+            rows="4"
+          ></textarea>
+
+          {errores.descripcion && (
+            <span className="error-mensaje">{errores.descripcion}</span>
+          )}
         </div>
 
+        <div>
+          <label>Calificación de la crítica</label>
+
+          <input
+            type="number"
+            name="calificacion"
+            min="1"
+            max="100"
+            value={videojuego.calificacion}
+            onChange={handleChange}
+          />
+
+          {errores.calificacion && (
+            <span className="error-mensaje">{errores.calificacion}</span>
+          )}
+        </div>
         <div className="checkbox">
           <label>
             <input
